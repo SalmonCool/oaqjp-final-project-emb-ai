b30faf28e0045ddd67c4446bba11703f45e82152
SalmonCool <samuelrcool2002@gmail.com>
2024-08-30 21:22:11 -0400

    Task 8 Pylint Inspection Completion

--- server.py
+++ server.py
@@ -1,25 +1,34 @@
-from EmotionDetection import emotion_detector
+"""
+This file manages a server that runs emotion detection
+
+Username: SalmonCool
+Date: August 30th 2024
+"""
+
 from flask import Flask, render_template, request
+from EmotionDetection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """ Function called when server launches that displays the 'index.html' template """
     return render_template('index.html')
 
 @app.route('/emotionDetector')
-def emotion_detector():
+def detect():
+    """ Calls the emotion_detector function to analyze the text given by the user"""
 
     #Get text from http request
     text = request.args.get('textToAnalyze','')
     result = emotion_detector(text)
 
     #Check for 'None' as dominant_emotion
-    if result['dominant_emotion'] == None:
+    if result['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
     #Continue as normal if no None present
-    result_text_format = "For the given statement, " \ 
+    result_text_format = "For the given statement, " \
     "the system response is 'anger': " + str(result['anger']) + "," \
     " 'disgust': " + str(result['disgust']) + "," \
     " 'fear': " + str(result['fear']) + "," \
